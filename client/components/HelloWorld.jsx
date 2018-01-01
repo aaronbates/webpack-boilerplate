d7f87412ec2efdb8a4198e61ae2eccb9ea3cfbf9
Aaron Bates <aaron@aaronbates.me>
2018-01-01 20:10:20 +0000

    feat: add React components to client

---
+++ client/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HelloWorld = ({ name }) => (
+  <div>{`Hi ${name}`}</div>
+);
+
+HelloWorld.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default HelloWorld;
